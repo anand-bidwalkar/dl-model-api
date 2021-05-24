57cb7e955c253a74d487c83d8be5fc2ad5e27ba9
abidwalkar <abidwalkar@deakin.edu.au>
2021-05-24 15:56:56 +1000

    Add index page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 modelfile = 'model/'
 model=tf.keras.models.load_model(modelfile)
 
+@app.route('/', methods=['POST'])
+def post():    
+    return "Welcome to D2I Geelong City Web API"
+
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
