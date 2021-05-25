dc9c89c76cbbfadc51d46f53127b305965c24498
abidwalkar <abidwalkar@deakin.edu.au>
2021-05-25 18:20:14 +1000

    Add AQI model enpoint

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
 import tensorflow as tf
 import os
+import joblib
 
 app = Flask(__name__)
-modelfile = 'model/'
-model=tf.keras.models.load_model(modelfile)
+
+real_estate_modelfile = 'model/real-estate'
+real_estate_model=tf.keras.models.load_model(real_estate_modelfile)
+
+aqi_modelfile = 'model/aqi/SVM_Final.pkl'
+aqi_model = joblib.load(aqi_modelfile)
 
 @app.route('/', methods=['POST'])
 def post():    
@@ -14,7 +19,13 @@ def post():
 @app.route('/api/', methods=['POST'])
 def makecalc():
     data = request.get_json()
-    prediction = str(model.predict(data)[0][0])
+    prediction = str(real_estate_model.predict(data)[0][0])
+    return {'Result': prediction}
+
+@app.route('/aqi_prediction/', methods=['POST'])
+def make_prediction():
+    data = request.get_json()
+    prediction = str(aqi_model.predict(data)[0])
     return {'Result': prediction}
 
 if __name__ == '__main__':
